c10c5a357e0d9a186c02d47945a2d0ac9f035033
mariamamunova <mariam493@nycstudents.net>
2025-09-25 15:41:26 -0400

    Add files via upload

---
+++ DogWalkCompany.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class DogWalkCompany
+{
+    private int[] schedule;
+    public DogWalkCompany()
+    {
+        schedule = new int[24];
+    }
+    public String toString()
+    {
+        String s = "";
+        for (int i = 0; i < schedule.length; i++)
+            s += i + " " + schedule[i] + "\n";
+        return s;
+    }
+    public int addDogs()
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.println("What hour?");
+        int hour = s.nextInt();
+        System.out.println("How many dogs?");
+        int dogs = s.nextInt();
+        schedule[hour] = dogs;
+        return hour;
+    }
+    /**
+     * Returns the number of dogs, always greater than 0, that are available
+     * for a walk during the time specified by hour
+     * Precondition: 0 <= hour <= 23
+     */
+    public int numAvailableDogs(int hour)
+    {
+        return schedule[hour];
+    }
+    /**
+     * Decreases, by numberDogsWalked, the number of dogs available for a walk
+     * during the time specified by hour
+     * Preconditions: 0 <= hour <= 23
+     * numberDogsWalked > 0
+     */
+    public void updateDogs(int hour, int numberDogsWalked)
+    {
+        schedule[hour] -= numberDogsWalked;
+    }
+}
---
+++ DogWalker.java
@@ -0,0 +1,36 @@
+public class DogWalker
+{
+    /**
+     * The maximum number of dogs this walker can walk simultaneously
+     * per hour
+     */
+    private int maxDogs;
+    /** The dog-walking company this dog walker is associated with */
+    private DogWalkCompany company;
+
+    /**
+     * Assigns max to maxDogs and comp to company
+     * Precondition: max > 0
+     */
+    public DogWalker(int max, DogWalkCompany comp)
+    {
+        maxDogs = max;
+        company = comp;
+    }
+    public int walkDogs(int hour)
+    {
+        int dogs = company.numAvailableDogs(hour);
+        if (dogs < maxDogs)
+        {
+            company.updateDogs(hour, dogs);
+            return dogs;
+        }
+        else
+        {
+            company.updateDogs(hour, maxDogs);
+            return maxDogs;
+        }
+
+    }
+
+}
